65f2bc1d617cbc9bda071d39598b8d69be597e94
JunaDev <juleannynavas@gmail.com>
2022-07-05 20:59:14 -0300

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Juleanny Navas Moreno on 05/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
