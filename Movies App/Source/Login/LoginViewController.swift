ce96bb091278dad547a1c4d9ac453d47d5c1f8d2
JunaDev <juleannynavas@gmail.com>
2022-08-10 23:35:11 -0300

    textFields data validation

--- Movies App/Source/Login/LoginViewController.swift
+++ Movies App/Source/Login/LoginViewController.swift
@@ -14,6 +14,13 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginSegmentedControl: UISegmentedControl!
     @IBOutlet weak var signInView: UIView!
     @IBOutlet weak var createAccountView: UIView!
+    @IBOutlet weak var caEmailTextField: UITextField!
+    @IBOutlet weak var caPasswordTextField: UITextField!
+    @IBOutlet weak var caCorfirmPasswordTextField: UITextField!
+    @IBOutlet weak var caButton: UIButton!
+    @IBOutlet weak var siEmailTextField: UITextField!
+    @IBOutlet weak var siPasswordTextField: UITextField!
+    @IBOutlet weak var siButton: UIButton!
 
 
     override func viewDidLoad() {
@@ -25,6 +32,13 @@ class LoginViewController: UIViewController {
     func setUpView() {
         signInView.isHidden = false
         createAccountView.isHidden = true
+        caButton.layer.cornerRadius = 6.0
+        caButton.layer.borderWidth = 1.0
+        caButton.layer.borderColor = UIColor.lightGray.cgColor
+        siButton.layer.cornerRadius = 6.0
+        siButton.layer.borderWidth = 1.0
+        siButton.layer.borderColor = UIColor.lightGray.cgColor
+
     }
 
     @IBAction func signInAction(_ sender: UISegmentedControl) {
@@ -35,5 +49,48 @@ class LoginViewController: UIViewController {
             signInView.isHidden = true
             createAccountView.isHidden = false
         }
+    }
+
+    @IBAction func validateSignInTextFieldsChanged(_ sender: UITextField) {
+        let email = siEmailTextField.text ?? ""
+        let password = siPasswordTextField.text ?? ""
+        if email.count > 0 && password.count > 0 {
+            siButton.isEnabled = true
+        } else {
+            siButton.isEnabled = true
+        }
+    }
+
+    @IBAction func validateCreateAccountTextFieldsChanged(_ sender: UITextField) {
+        let email = caEmailTextField.text ?? ""
+        let password = caPasswordTextField.text ?? ""
+        let confirmPassword = caCorfirmPasswordTextField.text ?? ""
+        if email.count > 0 && password.count > 0 && confirmPassword.count > 0 {
+            caButton.isEnabled = true
+        } else {
+            caButton.isEnabled = true
+        }
+    }
+
+    @IBAction func caActionButton(_ sender: UIButton) {
+
+    }
+
+    @IBAction func siActionButton(_ sender: UIButton) {
+        
+    }
+
+    func validateEmail(email: String) -> Bool {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: email)
+    }
+
+    func corfirmPassword(password1: String, password2: String) -> Bool {
+        return password1 == password2
+    }
+
+    func validatePasswordLong(password1: String) -> Bool {
+        return password1.count >= 8
     }
 }
