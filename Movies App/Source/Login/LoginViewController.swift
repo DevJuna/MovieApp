85ee12f0f8da4a86c4667998e8ff02bbaa1d2b94
JunaDev <juleannynavas@gmail.com>
2022-08-14 22:23:16 -0300

    Update config validation textFields

--- Movies App/Source/Login/LoginViewController.swift
+++ Movies App/Source/Login/LoginViewController.swift
@@ -20,7 +20,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var caPasswordTextField: UITextField!
     @IBOutlet weak var caPasswordImage: UIImageView!
     @IBOutlet weak var caPasswordLabel: UILabel!
-    @IBOutlet weak var caCorfirmPasswordTextField: UITextField!
+    @IBOutlet weak var caConfirmPasswordTextField: UITextField!
     @IBOutlet weak var caConfirmPasswordImage: UIImageView!
     @IBOutlet weak var caConfirmPasswordLabel: UILabel!
     @IBOutlet weak var caButton: UIButton!
@@ -31,6 +31,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var siPasswordImage: UIImageView!
     @IBOutlet weak var siPasswordLabel: UILabel!
     @IBOutlet weak var siButton: UIButton!
+
+    var isEnabledCAButton = false
+    var isEnabledSIButton = false
+
+    let validEmail = "Enter a valid email"
+    let formatPassword = "The password must contain 8 to 20 characters, including an uppercase letter, a lowercase letter and a number"
+    let confirmPassword = "Confirm your password"
+    let enterEmail = "Enter your email"
+    let enterPassword = "Enter your password"
+    let empty = String()
 
 
     override func viewDidLoad() {
@@ -43,104 +53,159 @@ class LoginViewController: UIViewController {
         signInView.isHidden = false
         createAccountView.isHidden = true
 
-        caEmailImage.image = UIImage(systemName: "mail.fill")
-        caEmailLabel.text = "Enter a valid email"
-        caPasswordImage.image = UIImage(systemName: "key.fill")
-        caPasswordLabel.text = "The password must contain 8 to 20 characters, including an uppercase letter, a lowercase letter and a number"
-        caConfirmPasswordImage.image = UIImage(systemName: "key.fill")
-        caConfirmPasswordLabel.text = "Confirm your password"
-
-
+        clearCreateAccount()
         caButton.layer.cornerRadius = 6.0
         caButton.layer.borderWidth = 1.0
         caButton.layer.borderColor = UIColor.lightGray.cgColor
 
-        siEmailImage.image = UIImage(systemName: "mail.fill")
-        siEmailLabel.text = "Enter your email"
-        siPasswordImage.image = UIImage(systemName: "key.fill")
-        siPasswordLabel.text = "Enter your password"
-
+        clearSignIn()
         siButton.layer.cornerRadius = 6.0
         siButton.layer.borderWidth = 1.0
         siButton.layer.borderColor = UIColor.lightGray.cgColor
-
     }
 
     @IBAction func segmentedControlAction(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
+            clearCreateAccount()
             signInView.isHidden = false
             createAccountView.isHidden = true
-        } else {
+        } else if sender.selectedSegmentIndex == 1 {
+            clearSignIn()
             signInView.isHidden = true
             createAccountView.isHidden = false
         }
     }
 
+    func clearCreateAccount() {
+        caEmailTextField.text = empty
+        caPasswordTextField.text = empty
+        caConfirmPasswordTextField.text = empty
+
+        caEmailImage.image = UIImage(systemName: "mail.fill")
+        caEmailImage.tintColor = UIColor(named: "#AAAAAA")
+        caEmailLabel.text = validEmail
+        caPasswordImage.image = UIImage(systemName: "key.fill")
+        caPasswordImage.tintColor = UIColor(named: "#AAAAAA")
+        caPasswordLabel.text = formatPassword
+        caConfirmPasswordImage.image = UIImage(systemName: "key.fill")
+        caConfirmPasswordImage.tintColor = UIColor(named: "#AAAAAA")
+        caConfirmPasswordLabel.text = confirmPassword
+    }
+
+    func clearSignIn() {
+        siEmailTextField.text = empty
+        siPasswordTextField.text = empty
+
+        siEmailImage.image = UIImage(systemName: "mail.fill")
+        siEmailImage.tintColor = UIColor(named: "#AAAAAA")
+        siEmailLabel.text = enterEmail
+        siPasswordImage.image = UIImage(systemName: "key.fill")
+        siPasswordImage.tintColor = UIColor(named: "#AAAAAA")
+        siPasswordLabel.text = enterPassword
+    }
+
+
     @IBAction func validateCreateAccountTextFieldsChanged(_ sender: UITextField) {
-        let email = caEmailTextField.text ?? ""
-        let password = caPasswordTextField.text ?? ""
-        let confirmPassword = caCorfirmPasswordTextField.text ?? ""
+        let email = caEmailTextField.text ?? empty
+        let password1 = caPasswordTextField.text ?? empty
+        let password2 = caConfirmPasswordTextField.text ?? empty
 
-        setUpEmailImageAndLabel(email: email, image: caEmailImage, label: caEmailLabel, textInfo: "Enter a valid email")
-
-
-        if email.count > 0 && password.count > 0 && confirmPassword.count > 0 {
-            caButton.isEnabled = true
-        } else {
-            caButton.isEnabled = true
-        }
+        setUpEmailImageAndLabel(email: email, image: caEmailImage, label: caEmailLabel, textInfo: validEmail)
+        setUpPasswordImageAndLabel(password: password1, image: caPasswordImage, label: caPasswordLabel, textInfo: formatPassword)
+        confirmPassword(password1: password1, password2: password2)
+        caButton.isEnabled = isEnabledCAButton
     }
 
     @IBAction func caActionButton(_ sender: UIButton) {
 
     }
 
+
     @IBAction func validateSignInTextFieldsChanged(_ sender: UITextField) {
-        let email = siEmailTextField.text ?? ""
-        let password = siPasswordTextField.text ?? ""
-        if email.count > 0 && password.count > 0 {
-            siButton.isEnabled = true
-        } else {
-            siButton.isEnabled = true
-        }
+        let email = siEmailTextField.text ?? empty
+        let password = siPasswordTextField.text ?? empty
+
+        setUpEmailImageAndLabel(email: email, image: siEmailImage, label: siEmailLabel, textInfo: enterEmail)
+        setUpPasswordImageAndLabel(password: password, image: siPasswordImage, label: siPasswordLabel, textInfo: enterPassword)
+        siButton.isEnabled = isEnabledSIButton
     }
-
-
 
     @IBAction func siActionButton(_ sender: UIButton) {
-        
+
     }
 
-    func validateEmail(email: String) -> Bool {
-        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
-        return emailPredicate.evaluate(with: email)
+
+    func isValid(data: String, regex: String) -> Bool {
+        let predicete = NSPredicate(format:"SELF MATCHES %@", regex)
+        return predicete.evaluate(with: data)
     }
 
     func setUpEmailImageAndLabel(email: String, image: UIImageView, label: UILabel, textInfo: String) {
         if email.isEmpty {
             image.image = UIImage(systemName: "mail.fill")
             image.tintColor = UIColor(named: "#AAAAAA")
+            label.text = textInfo
+            isEnabledCAButton = false
+            isEnabledSIButton = false
         } else {
-            if validateEmail(email: email) {
-                //Cambia la imagen a check verde y el texto ""
+            if isValid(data: email, regex: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}") {
                 image.image = UIImage(systemName: "checkmark.circle.fill")
                 image.tintColor = UIColor(named: "#40B617")
-                label.text = ""
+                label.text = empty
+                isEnabledCAButton = true
+                isEnabledSIButton = true
             } else {
-                //Cambia image a x rojo y el texto
                 image.image = UIImage(systemName: "x.circle.fill")
                 image.tintColor = UIColor(named: "#BE1D0C")
                 label.text = textInfo
+                isEnabledCAButton = false
+                isEnabledSIButton = false
             }
         }
     }
 
-    func corfirmPassword(password1: String, password2: String) -> Bool {
-        return password1 == password2
+    func setUpPasswordImageAndLabel(password: String, image: UIImageView, label: UILabel, textInfo: String) {
+        if password.isEmpty {
+            image.image = UIImage(systemName: "key.fill")
+            image.tintColor = UIColor(named: "#AAAAAA")
+            label.text = textInfo
+            isEnabledCAButton = false
+            isEnabledSIButton = false
+        } else {
+            if isValid(data: password, regex: "^(?=.[A-Z])(?=.[0-9])(?=.*[a-z]).{8,20}$") {
+                image.image = UIImage(systemName: "checkmark.circle.fill")
+                image.tintColor = UIColor(named: "#40B617")
+                label.text = empty
+                isEnabledCAButton = isEnabledCAButton && true
+                isEnabledSIButton = isEnabledSIButton && true
+            } else {
+                image.image = UIImage(systemName: "x.circle.fill")
+                image.tintColor = UIColor(named: "#BE1D0C")
+                label.text = textInfo
+                isEnabledCAButton = false
+                isEnabledSIButton = false
+            }
+        }
     }
 
-    func validatePasswordLong(password1: String) -> Bool {
-        return password1.count >= 8
+    func confirmPassword(password1: String, password2: String) {
+        if password2.isEmpty {
+            caConfirmPasswordImage.image = UIImage(systemName: "key.fill")
+            caConfirmPasswordImage.tintColor = UIColor(named: "#AAAAAA")
+            isEnabledCAButton = false
+        } else {
+            if password1 == password2 {
+                caConfirmPasswordImage.image = UIImage(systemName: "checkmark.circle.fill")
+                caConfirmPasswordImage.tintColor = UIColor(named: "#40B617")
+                caConfirmPasswordLabel.text = empty
+                isEnabledCAButton = isEnabledSIButton && true
+            } else {
+                caConfirmPasswordImage.image = UIImage(systemName: "x.circle.fill")
+                caConfirmPasswordImage.tintColor = UIColor(named: "#BE1D0C")
+                caConfirmPasswordLabel.text = confirmPassword
+                isEnabledCAButton = false
+            }
+        }
     }
+
 }
